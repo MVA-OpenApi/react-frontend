5b82053ed0c2cff2ac2d602bcaf46b51b9dfa4a3
Christian Szramek <christian.szramek@web.de>
2022-06-22 18:01:47 +0200

    made forms static

--- src/components/schemas/Employees.js
+++ src/components/schemas/Employees.js
@@ -3,37 +3,16 @@ import TextField from "@mui/material/TextField";
 
 import "../../styles/schemaform.css";
 
-const EmployeesSchema = {
-  Name: "Employees",
-  Properties: [
-    { Name: "name", Type: "string" },
-    { Name: "adress", Type: "string" },
-    { Name: "date_of_birth", Type: "string" },
-    { Name: "id", Type: "uuid.UUID" },
-  ],
-  XLabel: "form",
-};
-
 function Employees() {
   return (
     <form className="schema-form">
-      <h1 className="mobile-schema-name">{EmployeesSchema.Name}</h1>
-      {renderForm()}
+      <h1 className="mobile-schema-name">Employees</h1>
+      <TextField label="name" variant="outlined" />
+      <TextField label="adress" variant="outlined" />
+      <TextField label="date_of_birth" variant="outlined" />
+      <TextField label="id" variant="outlined" />
     </form>
   );
-}
-
-function renderForm() {
-  return EmployeesSchema.Properties.map((property, i) => {
-    return (
-      <TextField
-        id={property.Name + "textfield"}
-        label={property.Name}
-        variant="outlined"
-        key={i}
-      />
-    );
-  });
 }
 
 export default Employees;
--- src/components/schemas/Store.js
+++ src/components/schemas/Store.js
@@ -3,39 +3,18 @@ import TextField from "@mui/material/TextField";
 
 import "../../styles/schemaform.css";
 
-const StoreSchema = {
-  Name: "Store",
-  Properties: [
-    { Name: "adress", Type: "string" },
-    { Name: "annual_revenue", Type: "float32" },
-    { Name: "description", Type: "string" },
-    { Name: "id", Type: "uuid.UUID" },
-    { Name: "name", Type: "string" },
-    { Name: "size", Type: "int32" },
-  ],
-  XLabel: "form",
-};
-
 function Store() {
   return (
     <form className="schema-form">
-      <h1 className="mobile-schema-name">{StoreSchema.Name}</h1>
-      {renderForm()}
+      <h1 className="mobile-schema-name">Store</h1>
+      <TextField label="adress" variant="outlined" />
+      <TextField label="annual_revenue" variant="outlined" />
+      <TextField label="description" variant="outlined" />
+      <TextField label="id" variant="outlined" />
+      <TextField label="name" variant="outlined" />
+      <TextField label="size" variant="outlined" />
     </form>
   );
-}
-
-function renderForm() {
-  return StoreSchema.Properties.map((property, i) => {
-    return (
-      <TextField
-        id={property.Name + "textfield"}
-        label={property.Name}
-        variant="outlined"
-        key={i}
-      />
-    );
-  });
 }
 
 export default Store;
